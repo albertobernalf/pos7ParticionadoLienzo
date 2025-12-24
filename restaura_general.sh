a1967a255064581f24f7ef57a5a7b9d4f650c1b7
Alberto Bernal Ferreira <alberto.bernal@clinicamedical.local>
2025-12-24 14:42:01 -0500

    thrith commit

---
+++ restaura_general.sh
@@ -0,0 +1,1 @@
+/usr/pgsql-16/bin/psql -h localhost  -p 5432  -U postgres  -d vulner2 < /var/lib/pgsql/16/backups/aCentosGeneral.sql
---
+++ restaura_vulner.sh
@@ -0,0 +1,1 @@
+/usr/pgsql-16/bin/pg_restore -h localhost  -p 5432 -j 16 -U postgres  -d vulner -v "/var/lib/pgsql/16/backups/aCentos5.backup"
